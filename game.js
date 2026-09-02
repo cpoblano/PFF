37c0e7e80ef1c2c1347e47cf3addbebb4bcb38e5
Vael-10 <pobcarlos10@gmail.com>
2026-09-01 18:58:07 -0700

    123

---
+++ game.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+// Clear canvas with a color
+ctx.fillStyle = "#000000"; // Change this to your color
+ctx.fillRect(0, 0, canvas.width, canvas.height);
